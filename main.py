e26db7ebce11a0ea687d92a3bf663b715230ae1a
swathisureshh <167953947+swathisureshh@users.noreply.github.com>
2024-04-26 10:12:38 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from Vehicle import Bike, Car, Scooty
+
+def main():
+    bike = Bike("Red", 2, 5)
+    car = Car("Blue", 4, 6)
+    scooty = Scooty("Green", 2, 4)
+
+    bike.display_details()
+    print("\n")
+    car.display_details()
+    print("\n")
+    scooty.display_details()
+
+if __name__ == "__main__":
+    main()
+
