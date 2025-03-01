af31255d6938a374b49aeb9df974b4223c915381
swathisureshh <167953947+swathisureshh@users.noreply.github.com>
2025-03-01 21:52:34 +0530

    Add files via upload

--- recursion.py
+++ recursion.py
@@ -1,6 +1,31 @@
-def factorial(n):
-    if n==0:
-        return 1
-    else:
-        return n*factorial(n-1)
-print(factorial(5))
+# def series(n):
+#     if n==5:
+#         return n
+#     else:
+#         print(n)
+#         return series(n+1)
+# print(series(1))
+
+
+# def series(n):
+#     if n==1:
+#         return n
+#     else:
+#         print(n)
+#         return series(n-1)
+# print(series(5))
+
+# def proDigits(n):
+#     if  n==0 or n==1:
+#         return 1
+#     else:
+#         return n%10*proDigits(n//10)   
+# print(proDigits(984))
+
+
+# def factorial(n):
+#     if n==0 or n==1:
+#         return 1
+#     else:
+#         return n*factorial(n-1)   
+# print(factorial(7))
