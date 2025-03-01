b124aae24e81583ed88c1ac0e11f8dc9ff82cda8
swathisureshh <167953947+swathisureshh@users.noreply.github.com>
2025-03-01 21:51:41 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,18 @@
+class Student:
+    name="swathi"
+    roll=15
+    cgpa=9.46
+print(Student.name)
+print(Student.roll)
+
+
+
+# class Mobile:
+#     Ram='6gb'
+#     memory='128gb'
+#     name='Redminote 9'
+# print(Mobile.Ram)
+    
+s1=Student
+
+print(s1.name,s1.roll)
